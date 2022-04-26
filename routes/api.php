6e44a84889397139d20f32b3b04a35613bc250ee
Nurdiansah <nurdiansah14045@gmail.com>
2022-04-26 16:07:04 +0700

    Login Api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::namespace('Auth')->group(function () {
 Route::post('register', RegisterController::class);
+Route::post('login', LoginController::class);
+// });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
